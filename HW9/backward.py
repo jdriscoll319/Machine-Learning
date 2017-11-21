995fda5dd15be5a6f80564dfdb00790c5074eff0
Adam <jdriscol@andrew.cmu.edu>
2017-11-20 20:05:13 -0500

    working forward and backward algorithms!

---
+++ HW9/backward.py
@@ -0,0 +1,43 @@
+import sys
+####################################################################
+##REMOVE ME BEFORE SUBMITTING!!!!!!#################################
+####################################################################
+sys.path.append('./hw9-data')
+from logsum import log_sum
+import parse
+import math
+
+
+
+def backward(sentence_l, prior_l, trans_ll, emit_ld):
+    for sentence in sentence_l:
+        beta_t=[0]*8
+        sentence.reverse()
+        #print alpha_a
+        for index, word in enumerate(sentence):
+            if index != (len(sentence) - 1):
+                temp = [0] * 8
+                
+                for i in range(0, 8):
+                    sum_beta_a_b = -1e100
+                    
+                    for j in range(0,8):
+                        beta_a = beta_t[j] + math.log(trans_ll[i][j])
+                        beta_a_b = beta_a + math.log(emit_ld[j][word])
+                        sum_beta_a_b = log_sum(sum_beta_a_b, beta_a_b)
+                    
+                    temp[i] = sum_beta_a_b
+                
+                beta_t[:] = temp[:]  
+
+        sum_beta = -1e100
+        for i in range(0, 8):
+            beta = math.log( prior_l[i] ) + math.log( emit_ld[i][word] ) + beta_t[i]
+            sum_beta = log_sum(sum_beta, beta)
+        print sum_beta
+
+dev_l = parse.parse_dev(sys.argv[1])
+hmm_trans_ll = parse.parse_hmm_trans(sys.argv[2])
+hmm_emit_ld = parse.parse_hmm_emit(sys.argv[3])
+hmm_prior_l = parse.parse_hmm_prior(sys.argv[4])
+backward(dev_l, hmm_prior_l, hmm_trans_ll, hmm_emit_ld)
--- HW9/forward.py
+++ HW9/forward.py
@@ -16,34 +16,29 @@ def forward(sentence_l, prior_l, trans_ll, emit_ld):
         #print alpha_a
         for index, word in enumerate(sentence):
             ##calculate alpha1
-            ##convert these to logsum
             if index == 0:
-                print "FIRST WORD:"
                 for i in range(0,8):
-                    #print prior_l[i], emit_ld[i][word]
-                    ##alpha_t.append( math.log(prior_l[i]) + math.log(emit_ld[i][word]) )
-                    alpha_t.append( prior_l[i] * emit_ld[i][word] )
-                print alpha_t
-                print
+                    alpha_t.append( math.log(prior_l[i]) + math.log( emit_ld[i][word] ) )
+            
+            #calculate rest of the sentence
             else:
-                print "WORD" , index
                 temp = [0]*8
                 for i in range(0,8):
-                    print"  Alpha( state =" , i , ")"
-                    sum_alpha_a = math.exp(-100)
+                    sum_alpha_a = -1e100
                     
                     for j in range(0,8):
-                        #print alpha_a[i], alpha[i], trans_ll[j][i]
-                        alpha_a = alpha_t[j] * trans_ll[j][i]
-                        print "    Alpha*a =", alpha_a
-                        sum_alpha_a = log_sum(math.log(sum_alpha_a), math.log(alpha_a))
-                        print "    Summation:" , sum_alpha_a
+                        alpha_a = alpha_t[j] + math.log( trans_ll[j][i] )
+                        sum_alpha_a = log_sum( sum_alpha_a, alpha_a )
+                    
                     temp[i] = math.log(emit_ld[i][word]) + sum_alpha_a
-                    print "    Final Summation:" , temp[i]
+                
                 alpha_t[:] = temp[:]
-                print "  Alpha( t=" , index , ")", alpha_t
-        print math.fsum(alpha_t)
-        return 
+        
+        sum_alpha = -1e100
+        for i in range(0, 8):
+            sum_alpha = log_sum(sum_alpha, alpha_t[i])
+        print sum_alpha
+         
         
 
 dev_l = parse.parse_dev(sys.argv[1])
